5189e70a366a2b63601afcc8c8db0d67eb9b8f11
Arhychem <ananonymman@hotmail.com>
2024-07-06 13:37:09 +0100

    some modif

--- src/main/java/com/service/microservice/producer/ReviewPublisher.java
+++ src/main/java/com/service/microservice/producer/ReviewPublisher.java
@@ -4,24 +4,28 @@ import org.springframework.pulsar.core.PulsarTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.pulsar.core.PulsarTemplate;
 import org.springframework.stereotype.Service;
+
+import com.service.microservice.model.DriverReview;
+
 import lombok.extern.slf4j.Slf4j;
 
+@Service
+@Slf4j
 public class ReviewPublisher {
     private String topicName;
 
     private PulsarTemplate<Object> template;
 
-    public ReviewPublisher(@Value("${spring.pulsar.producer.topic-name1}") String topicName,
+    public ReviewPublisher(@Value("${spring.pulsar.producer.topic-name}") String topicName,
             PulsarTemplate<Object> template) {
         this.topicName = topicName;
         this.template = template;
     }
 
 
-    public void publishRawMessage(Review customer) {
-        template.send(topicName, customer);
-        System.out.println("sending message to the topic" + topicName2);
-        log.info("EventPublisher::publishRawMessage publish the event {}", customer.toString());
+    public void publishRawMessage(DriverReview review) {
+        template.send(topicName, review);
+        log.info("EventPublisher::publishRawMessage publish the event {}", review.toString());
     }
 
 }
